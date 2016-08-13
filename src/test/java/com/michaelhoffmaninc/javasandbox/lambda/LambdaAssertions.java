b17ee34ed771aaa1ccbe9b2bfd7c07f0a14429f7
Michael Hoffman <mike@michaelhoffmaninc.com>
2016-08-13 14:34:48 -0500

    Committing some lambda tests

---
+++ src/test/java/com/michaelhoffmaninc/javasandbox/lambda/LambdaAssertions.java
@@ -0,0 +1,82 @@
+package com.michaelhoffmaninc.javasandbox.lambda;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Tests out Lambda expressions
+ * 
+ * @author Michael Hoffman
+ *
+ */
+public class LambdaAssertions {
+
+   @Test
+   public void testFileFilterExample() throws Exception {
+
+      // Typically, we use anonymous classes to implement logic that can now be
+      // replaced with lambda expressions.
+      FileFilter oldWay = new FileFilter() {
+         @Override
+         public boolean accept(File file) {
+            // TODO Auto-generated method stub
+            return file.getName().endsWith(".java");
+         }
+      };
+
+      FileFilter newWay = (File file) -> file.getName().endsWith(".java");
+      File dir = new File(
+            "C:/dev/projects/javasandbox/src/test/java/com/michaelhoffmaninc/javasandbox/lambda");
+      File[] filesOldWay = dir.listFiles(oldWay);
+      File[] filesNewWay = dir.listFiles(newWay);
+      File[] filesNewWayCompact = dir
+            .listFiles((File file) -> file.getName().endsWith(".java"));
+
+      assertTrue(filesOldWay.length == 1);
+      assertTrue(filesNewWay.length == 1);
+      assertTrue(filesNewWayCompact.length == 1);
+   }
+
+   @Test
+   public void testCollectionComparator() throws Exception {
+      Comparator<String> oldComparator = new Comparator<String>() {
+
+         @Override
+         public int compare(String s1, String s2) {
+            return s1.compareTo(s2);
+         }
+
+      };
+
+      List<String> oldElements = Arrays.asList("pig", "apple", "bear", "dog", "cat",
+            "otter");
+      Collections.sort(oldElements, oldComparator);
+
+      assertEquals("apple", oldElements.get(0));
+      assertEquals("bear", oldElements.get(1));
+      assertEquals("cat", oldElements.get(2));
+      assertEquals("dog", oldElements.get(3));
+      assertEquals("otter", oldElements.get(4));
+      assertEquals("pig", oldElements.get(5));
+
+      List<String> newElements = Arrays.asList("pig", "apple", "bear", "dog", "cat",
+            "otter");
+      Collections.sort(newElements, (String s1, String s2) -> s1.compareTo(s2));
+
+      assertEquals("apple", newElements.get(0));
+      assertEquals("bear", newElements.get(1));
+      assertEquals("cat", newElements.get(2));
+      assertEquals("dog", newElements.get(3));
+      assertEquals("otter", newElements.get(4));
+      assertEquals("pig", newElements.get(5));
+   }
+}
